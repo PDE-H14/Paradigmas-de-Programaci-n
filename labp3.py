97a8d1a61efcc885a4a7bc2396a83465e32e2f4c
PDE-H14 <escamilla.huerta.porfirio@gmail.com>
2022-09-06 15:42:04 -0500

    Otro avance en funciones xdn, nos quieren sacar del salon

--- labp3.py
+++ labp3.py
@@ -157,7 +157,7 @@ print(salida)
 # Función con argumento #
 #-----------------------#
 
-def salu2(numbre):
+def salu2(nombre):
 	"""Esta función te saluda por tu nombre"""
 	print("¡Qué onda ese", nombre,"!")
 
@@ -171,10 +171,11 @@ salu2("Ángel")
 
 def saludos(nombre:str):
 	"""Esta función te saluda por tu nombre estrictamente"""
-	print("¡Qué onda ese", nombre,"!")
+	print("¡Qué onda ese", nombre,"!", "Type", type(nombre))
 saludos("Julián")
 a = 123
 print(type(a)) 
+saludos(a)
 
 #------------------------------#
 # Función con mchos argumentos #
@@ -197,4 +198,83 @@ def muchos_saludos(*nombres):
 	#---------------------------------#
 	print("Hola ", end="")
 	while len(nombres) > i:
-	
+	    #Último nombre
+            if (i == len(nombres)-1):
+                print(nombres[i])
+            else:
+                # Cualquier otro nombre
+                print(nombres[i], end= ", ")
+            i += 1
+
+muchos_saludos("Bosco", "Angel", "David", "Tamara", "Mili", "Edwin", "Lev", "Luis", "Abigail")
+
+def greet(firstName, lastName):
+    print("Hello", firstName, lastName)
+
+#----------------------------------------------#
+# Llamar la función con argumentos en desorden #
+#----------------------------------------------#
+
+greet(lastName = "Jobs", firstName = "Steve") # Se pueden especificar las variables en desorden
+
+#-----------------------------------#
+# Función con argumentos escondidos #
+#-----------------------------------#
+
+def greet(**person):
+
+    #-----------------------------------------------------#
+    # Persona tiene caracteristicas: firstName y lastName #
+    #-----------------------------------------------------#
+
+    print("Hello", person["firstName"], person["lastName"])
+
+greet(firstName = "Manolo", lastName = "conchas")
+greet(lastName = "Conchas", firstName = "Manolo")
+greet(firstName = "Manolo", lastName = "Conchas", age = 55) # Se pueden pasar mas parametros de los necesarios
+
+#---------------------------------#
+# Función con valores por defecto #
+#---------------------------------#
+
+def greet(name = "Guest"):
+    print("Hello, ", name)
+
+greet() # Utiliza el valor por defecto
+greet("Steve")
+
+#-----------------------#
+# Función con resultado #
+#-----------------------#
+
+def suma(a,b):
+    return a+b
+
+print(suma(2,3))
+
+#---------------------------------------------------------------#
+# Programación funcional. Se pueden usar funciones de funciones #
+#---------------------------------------------------------------#
+
+total = suma(5, suma(10,20))
+print(total)
+
+#--------------------------------------------------#
+#                  Cálculo llamado                 #
+# nombre de la función = lambda vaiables : función #
+#--------------------------------------------------#
+
+x_to_2 = lambda x : x * x
+a1 = x_to_2(5)
+print(a1)
+
+#----------------------------#
+# Lambda de varias variables #
+#----------------------------#
+
+suma = lambda x1, x2, x3: x1 + x2 + x3
+print(suma(1,2,3))
+
+sumas = lambda *x: x[0]+x[1]+x[2]+x[3]
+print(sumas(1,2,3,4))
+
