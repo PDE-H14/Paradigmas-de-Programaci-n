6c436055dcea3b30b22529c6762d5e174d26a3a6
PDE-H14 <73668458+PDE-H14@users.noreply.github.com>
2022-11-10 12:15:26 -0600

    Add files via upload
    lab 6 main

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from aplicacion.banco.cliente_bancario import ClienteBancario
 
 #--------------------------------------------#
-#  try: intenta ejecutar las instrucciones   #
+# try: intenta (correr las instrucciones)    #
 # except: atrapar el error en una variable e #
-#     e se puede convertir a string          #
+# e se puede convertir a String              #
 #--------------------------------------------#
 
-#------------------------------------------#
-# Error por sacar más dinero del que tiene #
-#------------------------------------------#
+try:
+    cliente = ClienteBancario("Jaime Andrade", "Hernández Sánchez", 28, 0.0)
+    cliente.guardarDinero(300)
+    print(cliente)
+    cliente.retirarDinero(400)
+    print(cliente)
+#---------------------------------------------#
+# Exception es el objeto más general de error #
+#---------------------------------------------#
+
+except Exception as e:
+    print("Error "+str(e))
+
+#------------------------------------#
+# Error por usar un atributo privado #
+#------------------------------------#
 
 try:
-	cliente = ClienteBancario("Jaime Andrade", "Hernandez Sánchez", 28, 0.0)
-	cliente.guardarDinero(300)
-	print(Cliente.imprimirInfo())
-	print(Cliente)
-	cliente.retirarDinero(400)
-	print(Cliente.imprimirInfo())
-	print(Cliente)
+    print(cliente.__nombres)
+except Exception as e:
+    print("Error "+str(e))
 
+#----------------#
+# Forma correcta #
+#----------------#
+
+print(cliente.nombres)
