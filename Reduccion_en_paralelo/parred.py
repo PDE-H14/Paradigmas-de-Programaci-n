1536f619db5e1b8187a4aea47bc10f653bb14133
PDE-H14 <escamilla.huerta.porfirio@gmail.com>
2023-01-05 15:51:22 -0600

    Primer ronda de cambios de nombre

---
+++ Reduccion_en_paralelo/parred.py
@@ -0,0 +1,14 @@
+#--------------------------------------#
+# Reducción unidimendional en paralelo #
+#--------------------------------------#
+
+from numba import *
+import numpy as np
+
+# Número de celdas 
+n: int64 = 512
+pasos: int = 2 
+
+def tridente(vector, nElements, index, pow2):
+	v_i = v(i)
+	if i
